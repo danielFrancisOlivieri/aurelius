3afe8358ab707362e97bf5e31cc45899fe1f41f3
danielFrancisOlivieri <dfolivieri@gmail.com>
2021-07-31 13:28:14 -0400

    Added nvmrc file
    Hopefully will solve the build error.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,6 +15,7 @@ On a cave tour in Iceland, our tour guide told us how when you find someone who�
 There was something mischievous about her smile when she told us, “I am about to turn off the light so that you can experience total cave darkness.” It felt like total cave darkness was an eccentric aunt who she wanted to introduce us to. I was eager to meet her. To go your whole life without “seeing” total cave darkness would be like never seeing the ocean. When she turned off the light the cave turned on its darkness. No one spoke in the same way no one speaks during prayers in a church. I blinked and registered no change. Eyes opened or closed, I saw the same thing. While the flashlight had been on, I had the feeling of being in a group. There were at least 15 or 20 of us on the tour. But when the light went out, it took the other people with it. Philosophers bat around the idea of solipsism—that we are all marooned on the islands of ourselves, unable to prove once and for all that anyone else exists. Alone in the universe. Standing silent in total cave darkness, it is not a theory. It’s a reality. But at this moment of total isolation, you’ve actually come across one of the few human experiences that millenia have done nothing to alter. The utter darkness I saw in that cave is the same darkness the mathematician and philosopher Pythagoras saw went he descended a cave to discover Hades. The same darkness that Mayan peoples offered sacrifices of precious pottery to. The same darkness that the scientist Michel Siffre saw in his two month long sensory deprivation study. The same darkness that indigenous Australian have been visiting for hundred of years to mine sacred ochre from. The same darkness that the Lakota of the Black Hills believe the first humans emerged out of. This is the place humans across history have gone to be away from places. It satisfies our, “eternal desire to connect with what we cannot see.” Where our vision no longer serves us, our minds start to take over. All day we have our eyes feeding reality to us, overshadowing our other senses—what happens to us when you remove it? It submerges you not in your senses, but in your mind. 
 They give what all void gives which is the chance, no, the challenge to look deeper into your mind because they offer no escape from it into the distractions of vision. That the deepest vision you can find is no vision; the most powerful experience of sight is to not see. Fitzgerald wrote, “The strongest guard is placed at the gateway to nothing. Maybe because the condition of emptiness is too shameful to be divulged.” What happens when you slip past those guards? You see that the buried treasure down in the depths of the cave is no treasure at all, is the chance to content with emptiness and that is the wonder, the boon, the loot, the revelation you must bring back with you into the light.
 `;
+
 const fullText = newText;
 const EventStage = {
   HASNOTBEGUN: 1,
@@ -29,7 +30,6 @@ export class Home extends React.Component {
       title: 'Dark Mode',
       fullText: newText,
       textIndexNumber: 3,
-      //   ExplanationTextStatus: true,
       fullTextArray: fullText.split(' '),
       textPresented: explanatoryText,
       firstBackwardsStatus: EventStage.HASNOTBEGUN,
@@ -47,7 +47,6 @@ export class Home extends React.Component {
   }
 
   returnTextToPresent(fullTextArray, textIndexNumber) {
-    // Console.log(fullTextArray);
     let toReturn = '';
     for (let i = 0; i < textIndexNumber; i++) {
       toReturn += fullTextArray[i] + ' ';
@@ -97,10 +96,10 @@ export class Home extends React.Component {
   }
 
   handleTouchDown(e) {
-    if ((this.state.firstBackwardsStatus === EventStage.HASNOTBEGUN) && (this.state.textIndexNumber > 634)) {
+    if ((this.state.firstBackwardsStatus === EventStage.HASNOTBEGUN) && (this.state.textIndexNumber > 6340)) {
       this.subtractWord(e);
       this.setState({firstBackwardsStatus: EventStage.BEGUN});
-    } else if ((this.state.firstBackwardsStatus === EventStage.BEGUN) && (this.state.textIndexNumber < 597)) {
+    } else if ((this.state.firstBackwardsStatus === EventStage.BEGUN) && (this.state.textIndexNumber < 5970)) {
       this.setState({firstBackwardsStatus: EventStage.FINISHED});
     } else if (this.state.firstBackwardsStatus === EventStage.BEGUN) {
       this.subtractWord(e);
@@ -111,10 +110,10 @@ export class Home extends React.Component {
 
   handleKeyDown(e) {
     this.changeBackgroundBrightness();
-    if ((this.state.firstBackwardsStatus === EventStage.HASNOTBEGUN) && (this.state.textIndexNumber > 634)) {
+    if ((this.state.firstBackwardsStatus === EventStage.HASNOTBEGUN) && (this.state.textIndexNumber > 6340)) {
       this.subtractWord(e);
       this.setState({firstBackwardsStatus: EventStage.BEGUN});
-    } else if ((this.state.firstBackwardsStatus === EventStage.BEGUN) && (this.state.textIndexNumber < 597)) {
+    } else if ((this.state.firstBackwardsStatus === EventStage.BEGUN) && (this.state.textIndexNumber < 5970)) {
       this.setState({firstBackwardsStatus: EventStage.FINISHED});
     } else if (this.state.firstBackwardsStatus === EventStage.BEGUN) {
       this.subtractWord(e);
